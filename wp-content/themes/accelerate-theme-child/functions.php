08f80dc04132de1786ceed14dccbd71a596926e3
Harlee Tholen <harleetholen@gmail.com>
2019-09-02 15:51:25 -0600

    customized the child theme and added pages to it.

---
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
+
+// Enqueue scripts and styles
+function accelerate_child_scripts(){
+	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+// Custom post types functions
+//create a case study custom post type
+function create_custom_post_types() {
+	register_post_type('case_studies',
+		array(
+			'labels' => array(
+				'name' => _( 'Case Studies' ),
+				'singular_name' => _( 'Case Study')
+				),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array(
+				'slug' => "case-studies" ),
+			)
+		);
+}
+// Hook this custom post type function into the theme
+add_action( 'init', 'create_custom_post_types' );
